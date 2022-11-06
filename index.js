7cc8310c706bd9bfed4864efb7c4967edee701e2
Azrubael <i0638464000@gmail.com>
2022-11-06 17:14:27 +0200

    intermediate variant with Error 404 handle

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ console.log(chalk.magenta(__filename))
 // Немного поигрался с цветным выводом в консоль
 // Теперь создаются сервера 'http'
 const server = http.createServer((req, res) => {
-    // Ответ сервера о том, что все Ок
-    if (req.url === '/') {
+
+/* Простейшее отображение двустраниц, см. первый коммит
+     if (req.url === '/') {
         fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
             if (err) { throw err }
         res.writeHead(200, { 'Content-Type': 'text/html' })
@@ -27,7 +28,33 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, { 'Content-Type': 'text/html' })
         res.end(data)
         })
+    } */
+
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url)
+    const ext = path.extname(filePath)
+    if (!ext) {
+        filePath += '.html'
     }
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            fs.readFile(path.join(__dirname, 'public', 'error.html'), (err, data) => {
+                if (err) {
+                    res.writeHead(500)
+                    res.end('Error 500')
+                } else {
+                    res.writeHead(200, { 'Content-Type': 'text/html' })
+                    res.end(data)
+                }
+            })
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            res.end(content)
+        }
+    })
+
+/*     это завершение требуется только для первого коммита
+    res.end()  */
 
     // вывод в консоль путей, после загрузки домашней страницы
     console.log(req.url)
