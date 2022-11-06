3e380e785d9729ee3b8c872f463e0f1d6097de40
Azrubael <i0638464000@gmail.com>
2022-11-06 17:39:04 +0200

    final for that lesson

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
+/*  Запуск простого HTTP сервера без применения библиотеки Express, Koa,
+    Hapi или какого-то фреймворка.
+    Как видно, требуется автоматизировать достаточно больщой объем работы,
+    который становится тем значительнее, чем сожнее структура сервера SPA
+    (Single Page Application)
+*/
+
 const chalk = require('chalk')
 const fs = require('fs')
 const path = require('path')
 const ttt = require('./data.js')
 const http = require('http')
-const PORT = 9001
+const PORT = process.env.PORT || 9001
 
+// Немного поигрался с цветным выводом в консоль
 console.log(chalk.blue('Hello NodeJS'))
 console.log(chalk.green(ttt))
 console.log(chalk.red(__dirname))
 console.log(chalk.magenta(__filename))
 
-// Немного поигрался с цветным выводом в консоль
+
 // Теперь создаются сервера 'http'
 const server = http.createServer((req, res) => {
 
@@ -32,6 +40,19 @@ const server = http.createServer((req, res) => {
 
     let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url)
     const ext = path.extname(filePath)
+    let contentType = 'text/html'
+
+    switch (ext) {
+        case '.css':
+            contentType = 'text/css'
+            break
+        case '.js':
+            contentType = 'text/javascript'
+            break
+        default:
+            contentType = 'text/html'
+    }
+
     if (!ext) {
         filePath += '.html'
     }
@@ -43,12 +64,12 @@ const server = http.createServer((req, res) => {
                     res.writeHead(500)
                     res.end('Error 500')
                 } else {
-                    res.writeHead(200, { 'Content-Type': 'text/html' })
+                    res.writeHead(200, { 'Content-Type': contentType })
                     res.end(data)
                 }
             })
         } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' })
+            res.writeHead(200, { 'Content-Type': contentType })
             res.end(content)
         }
     })
@@ -63,5 +84,5 @@ const server = http.createServer((req, res) => {
 })
 
 server.listen(PORT, () => {
-    console.log(`На ${PORT} запущен сервер...`)
+    console.log(`Через порт ${PORT} запущен сервер...`)
 })
